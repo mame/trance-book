29e7e745d0ed6eb5fd486889f4b080cf4397fb2f
Yusuke Endoh <mame@ruby-lang.org>
2015-09-24 02:44:48 +0900

    gh-pages を submodule にしない

--- publish.sh
+++ publish.sh
@@ -1,8 +1,7 @@
+git clone -b gh-pages git@github.com:mame/trance-book.git gen
 ruby setup-demo.rb
 bundle exec jekyll build
 cd gen
 git add -A .
 git commit -m "sync"
 git push origin gh-pages
-cd ..
-git add gen
