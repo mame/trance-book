cd78140082d4df4a4b3170e26625fa2ac43977c8
Yusuke Endoh <mame@ruby-lang.org>
2015-09-24 03:10:13 +0900

    ファイルパス修正

--- vendor/scripts/0-demo.bash
+++ vendor/scripts/0-demo.bash
@@ -2,7 +2,7 @@
 source $(dirname $BASH_SOURCE)/config.sh
 
 mkdir -p $VENDOR/jor1k/utils/packages/
-cd $VENDOR/../demo/extended-fs
+cd $VENDOR/../jor1k-fs/extended-fs
 tar cf $VENDOR/jor1k/utils/packages/trance-demo.tar --owner=1000 --group=1000 home/
 tar rf $VENDOR/jor1k/utils/packages/trance-demo.tar --owner=0 --group=0 usr
 rm -f $VENDOR/jor1k/utils/packages/trance-demo.tar.bz2
